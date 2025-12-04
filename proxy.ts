c5aa82621731a7a7bb335890efff999295808a26
ljreaux <146141261+ljreaux@users.noreply.github.com>
2025-12-03 21:34:49 -0600

    upgraded from middleware to proxy per rec

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { i18nRouter } from "next-i18n-router";
 import i18nConfig from "@/i18nConfig";
 import { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   return i18nRouter(request, i18nConfig);
 }
 
